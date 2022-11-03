237fd3279d42bfadf918e60ed071de2cf5b5cdc3
KenanAkbarli <tu6m5m78u@code.edu.az>
2022-11-03 11:08:14 +0400

    feat: add Algorithm3 task with c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Drop the constants
+int[] array = new int[] {1, 2, 3, 4};
+int[] newArr = array;
+int i;
+int j;
+Console.WriteLine("Donguden evvel");
+for (i = 0; i < newArr.Length; i++)     //O(n)
+{
+    Console.WriteLine(newArr[i]);       //O(1)
+}
+Console.WriteLine("Donguden sonra");
+for (j = 0; j < newArr.Length; j++)     //O(n)
+{
+    Console.WriteLine(newArr[j]+1);     //O(1)
+}
+
+//This is O(2n), which we just call O(n).
